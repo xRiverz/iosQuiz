3e658844e68ae8679ad38074737b4f4637fcd3ee
xRiverz <anhargalam@gmail.com>
2021-12-02 10:41:07 +0000

    Initial Commit

---
+++ iosQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosQuiz
+//
+//  Created by admin on 02/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
